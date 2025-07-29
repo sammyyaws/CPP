bebb9861f67fa26a681a8656ccb1a005644142dc
Engineer Sam <sammyyawsnana1@gmail.com>
2025-07-29 20:12:38 +0000

    Update:changing from vector array to use database instead

---
+++ build/CMakeFiles/LIBMAS.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/sammyaws/Desktop/LIBMAS/Librarian.cpp" "CMakeFiles/LIBMAS.dir/Librarian.cpp.o" "gcc" "CMakeFiles/LIBMAS.dir/Librarian.cpp.o.d"
+  "/home/sammyaws/Desktop/LIBMAS/Main.cpp" "CMakeFiles/LIBMAS.dir/Main.cpp.o" "gcc" "CMakeFiles/LIBMAS.dir/Main.cpp.o.d"
+  "/home/sammyaws/Desktop/LIBMAS/User.cpp" "CMakeFiles/LIBMAS.dir/User.cpp.o" "gcc" "CMakeFiles/LIBMAS.dir/User.cpp.o.d"
+  "" "LIBMAS" "gcc" "CMakeFiles/LIBMAS.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/LIBMAS.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LIBMAS.dir/link.d"
+  "CMakeFiles/LIBMAS.dir/Librarian.cpp.o"
+  "CMakeFiles/LIBMAS.dir/Librarian.cpp.o.d"
+  "CMakeFiles/LIBMAS.dir/Main.cpp.o"
+  "CMakeFiles/LIBMAS.dir/Main.cpp.o.d"
+  "CMakeFiles/LIBMAS.dir/User.cpp.o"
+  "CMakeFiles/LIBMAS.dir/User.cpp.o.d"
+  "LIBMAS"
+  "LIBMAS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LIBMAS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
